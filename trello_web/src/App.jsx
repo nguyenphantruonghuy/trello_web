673552f07cb4ee27c4c94187c91c9b2f14a5dc09
truonghuy2004-admin <atkhuyzzz@gmail.com>
2026-03-25 15:58:28 +0700

    theming

--- trello_web/src/App.jsx
+++ trello_web/src/App.jsx
@@ -2,6 +2,7 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import Typography from '@mui/material/Typography';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <div>huyhehe</div>
+    <Typography variant='body2' color="text.secondary">test typography</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
--- trello_web/src/main.jsx
+++ trello_web/src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import Theme from './theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
+ 
